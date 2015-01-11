4248c2c0b27d8ab0e4e78878d5971cfd1ed48551
Metric-nz <metric.nz@gmail.com>
2015-01-12 11:55:42 +1300

    Sample code for editting PHP timezone

--- files/provision_dev.sh
+++ files/provision_dev.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
 
+for f in /etc/php5/cli/php.ini /etc/php5/apache2/php.ini
+do
+  sed -i "s/;date.timezone =/date.timezone = Europe\/Paris/" $f
+  echo "xdebug.max_nesting_level=250" >> $f
+done  
